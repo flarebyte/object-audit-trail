c5e9d68806e3bb1b875794c48e6808e4405393ac
olih <olih@users.noreply.github.com>
2016-10-30 16:24:02 +0000

    Retrieve attribution from history

--- src/index.js
+++ src/index.js
@@ -1,7 +1,82 @@
+import _ from "lodash"
+
 /**
  * @param {Type}
  * @return {Type}
  */
-export default function () {
-  return true
+export default function (conf) {
+  const shorterOfTwo = (first, second) => {
+    if (_.isNil(second)) {
+      return first;
+    }
+    return _.size(first) < _.size(second) ? first : second;
+  };
+
+  const largestPossible = (list, maxSize) => {
+    const sizeOrZero = (n) => {
+      return _.size(n)> maxSize ? 0: _.size(n);
+    }
+    const largest = _.maxBy(list, sizeOrZero);
+    return sizeOrZero(largest) === 0 ? null : largest;
+  }
+
+  const isLicensedUnder = " is licensed under "
+  const getIdealAttributionAsText = (history) => {
+    const last = _.last(history);
+    const headline = last.headline;
+    const author = last.author.name;
+    const license = last.license.name;
+    const under = isLicensedUnder;
+    const attribution = `${headline} by ${author}${under}${license}`;
+    return attribution;
+  }
+
+  const getShorterAttributionAsText = (history, limit) => {
+    const last = _.last(history);
+    const author = shorterOfTwo(last.author.name, last.author.alternateName);
+    if (_.size(author)+3 >= limit) {
+      return null;
+    }
+    const license = shorterOfTwo(last.license.name, last.license.alternateName);
+    const under = " / ";
+    const attributionCredit = `by ${author}${under}${license}`;
+    if (_.size(attributionCredit) > limit) {
+      return `By ${author}`;
+    }
+    const maxHeadline = limit - 1 - _.size(attributionCredit);
+    const headline = largestPossible([
+      last.headline,
+      last.alternativeHeadline,
+      last.typeOfWork.name
+    ], maxHeadline);
+
+    if (_.isNil(headline)) {
+      return _.upperFirst(attributionCredit);
+    }
+    const attribution = `${headline} by ${author}${under}${license}`;
+    return attribution;
+  }
+
+
+  const getAttributionAsText = (history , limit) => {
+    const defaultAttrib = getIdealAttributionAsText(history);
+    const useDefault = _.isNil(limit) || _.size(defaultAttrib) <= limit;
+    return useDefault ? defaultAttrib :
+    getShorterAttributionAsText(history, limit);
+  }
+  const getAttributionAsMarkdown = (history, limit) => {
+    return "";
+  }
+  const getTwitterAttribution = (history) => {
+    return "";
+  }
+
+  const objectAuditTrail = {
+    shorterOfTwo,
+    largestPossible,
+    getAttributionAsText,
+    getAttributionAsMarkdown,
+    getTwitterAttribution
+  }
+  return objectAuditTrail;
 }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,116 @@
-import test from "tape"
-import objectAuditTrail from "../src"
+import test from 'tape'
+import objectAuditTrail from '../src'
+const oneAuthor = require('./one-author.json')
 
-test("objectAuditTrail", (t) => {
+/*
+See https://wiki.creativecommons.org/wiki/Best_practices_for_attribution
+*/
+
+const tested = objectAuditTrail();
+
+test('objectAuditTrail should detect the shorter of two words', (t) => {
+  t.plan(2)
+  t.equal(tested.shorterOfTwo('ABCDE','ABC'), 'ABC', 'ABC')
+  t.equal(tested.shorterOfTwo('Single'), 'Single', 'Single')
+})
+
+test('objectAuditTrail should detect the largest possible words', (t) => {
+  t.plan(4)
+  t.equal(tested.largestPossible(['ABCDE','ABC', 'abcdef'], 3), 'ABC', '3')
+  t.equal(tested.largestPossible(['ABCDE','ABC', 'abcdef'], 5), 'ABCDE', '5')
+  t.equal(tested.largestPossible(['ABCDE','ABC', 'abcdef'], 10), 'abcdef', '10')
+  t.equal(tested.largestPossible(['ABCDE','ABC', 'abcdef'], 2), null, 'no')
+
+})
+
+
+test('objectAuditTrail should provide attribution', (t) => {
   t.plan(1)
-  t.equal(true, objectAuditTrail(), "return true")
+  const actual = tested.getAttributionAsText([oneAuthor])
+  t.equal(actual,
+  'London gathering by John Smith is licensed under CC BY 4.0',
+  'as text')
+})
+
+test('objectAuditTrail should provide attribution within large limit', (t) => {
+  t.plan(1)
+  const actual = tested.getAttributionAsText([oneAuthor], 100)
+  t.equal(actual,
+  'London gathering by John Smith is licensed under CC BY 4.0',
+  'as text with large limit')
+})
+
+test('objectAuditTrail should provide compact attribution', (t) => {
+  t.plan(1)
+  const actual = tested.getAttributionAsText([oneAuthor], 40)
+  t.equal(actual,
+  'London gathering by John Smith / CC BY',
+  'compact license')
+})
+
+
+test('objectAuditTrail should provide short attribution', (t) => {
+  t.plan(1)
+  const actual = tested.getAttributionAsText([oneAuthor], 35)
+  t.equal(actual,
+  'Comic Script by John Smith / CC BY',
+  'as short text')
+
+})
+
+test('objectAuditTrail should provide attribution with just author and license',
+ (t) => {
+  t.plan(1)
+  const actual = tested.getAttributionAsText([oneAuthor], 22)
+  t.equal(actual,
+  'By John Smith / CC BY',
+  'with just author and license')
+
+})
+test('objectAuditTrail should provide attribution with just author', (t) => {
+  t.plan(1)
+  const actual = tested.getAttributionAsText([oneAuthor], 15)
+  t.equal(actual,
+  'By John Smith',
+  'with just author')
+})
+
+test('objectAuditTrail should return empty null limit is too small ', (t) => {
+  t.plan(1)
+  const actual = tested.getAttributionAsText([oneAuthor], 5)
+  t.equal(actual,
+  null,
+  'limit is too small')
+})
+
+test('objectAuditTrail should provide markdown attribution', (t) => {
+  t.plan(1)
+  const actual = tested.getAttributionAsMarkdown([oneAuthor])
+  t.equal(actual,
+  '[London gathering](/comic-script/AAA) by [John Smith](/john-smith)'
+  + 'is licensed under '+
+  '[CC BY 4.0](https://creativecommons.org/licenses/by/2.0/)',
+  'as markdown')
+
+})
+
+test('objectAuditTrail should provide short markdown attribution', (t) => {
+  t.plan(1)
+  const actual = tested.getAttributionAsMarkdown([oneAuthor], 35)
+  t.equal(actual,
+  '[Comic Script](/comic-script/AAA) by [John Smith](/john-smith)'
+  + ' / '+
+  '[CC BY](https://creativecommons.org/licenses/by/2.0/)',
+  'as short markdown')
+
+})
+
+test('objectAuditTrail should provide twitter attribution', (t) => {
+  t.plan(1)
+  const actual = tested.getTwitterAttribution([oneAuthor])
+  t.equal(actual,
+  'London gathering by @johnsmith #CCBY : '+
+  'http:/website.com/comic-script/AAA',
+  'as twitter')
+
 })
