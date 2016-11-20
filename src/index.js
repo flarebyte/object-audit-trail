7f1c55199fdb572c2d96555d3caaf8d97377b8f0
olih <olih@users.noreply.github.com>
2016-11-20 09:52:40 +0000

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import _ from 'lodash';
 import pickAltVal from 'pick-alternate-value';
 
+const defTextTemplates = ['{{headline}} by {{author}} is licensed under {{license}}',
+  '{{headline}} by {{author}} / {{license}}',
+  'By {{author}} / {{license}}', 'By {{author}}'];
+
+const defMdTemplates = ['[{{headline}}]({{headlineUrl}}) by' +
+'[{{author}}]({{authorUrl}}) is licensed under [{{license}}]({{licenseUrl}})',
+  '[{{headline}}]({{headlineUrl}}) by' +
+'[{{author}}]({{authorUrl}}) / [{{license}}]({{licenseUrl}})',
+  'By [{{author}}]({{authorUrl}}) / [{{license}}]({{licenseUrl}})',
+  'By [{{author}}]({{authorUrl}})'];
+
+const defProps = {
+  author: ['author.name', 'author.alternateName'],
+  license: ['license.name', 'license.alternateName'],
+  headline: ['headline', 'alternativeHeadline'],
+  typeOfWork: ['typeOfWork.name'],
+};
+
+
+const defTextPlaceholders = {
+  clean: [],
+  extract: [['{{', '}}']],
+};
+
+const defMdPlaceholders = {
+  clean: [['({{', '}}])']],
+  extract: [['[{{', '}}]'], ['({{', '}}])']],
+};
+
 /**
  * @param {Type}
  * @return {Type}
@@ -28,90 +57,9 @@ export default function (conf) {
     return edition;
   };
 
-  const blk = str => pickAltVal.discardPlaceholders(str);
-  const quote = value => `"${value}"`;
-
-  const isLicensedUnder = ' is licensed under ';
-  const byAuthorTempl = 'By ${author}';
-  const byAuthorTemplMd = 'By [${author}](${authorUrl})';
-  const byAuthorAndLicenseTempl = 'By ${author}${under}${license}';
-  const byAuthorWithHeadlineTempl = '${headline} by ${author}${under}${license}';
-
-  const altUnder = [isLicensedUnder, ' / ', '/'];
-
-  const rankFn = list => pickAltVal.sumSize(list);
-
-  const runByAuthorTempl = (list) => {
-    const params = {
-      author: list[1],
-    };
-    return _.template(byAuthorTempl)(params);
-  };
-
-  const runByAuthorTemplMd = (list) => {
-    const params = {
-      author: list[1],
-      authorUrl: list[2],
-    };
-    return _.template(byAuthorTemplMd)(params);
-  };
-
-  const runByAuthorAndLicenseTempl = (list) => {
-    const params = {
-      author: list[1],
-      under: list[2],
-      license: list[3],
-    };
-    return _.template(byAuthorAndLicenseTempl)(params);
-  };
-
-  const runByAuthorWithHeadlineTempl = (list) => {
-    const params = {
-      headline: list[1],
-      author: list[2],
-      under: list[3],
-      license: list[4],
-    };
-    return _.template(byAuthorWithHeadlineTempl)(params);
-  };
-
-
   const getSingleAuthorAttributionAsText = (history, opts) => {
     const limit = _.get(opts, 'limit', 10000);
-    const filterFn = list => pickAltVal.hasNoNull(list) &&
-    pickAltVal.sumSize(list) <= limit;
-
-    const bestOf = list => pickAltVal.highestRankedCombination(
-      pickAltVal.combineListOfList(list), rankFn, filterFn);
-
-    const last = _.last(history);
-    const altAuthor = [last.author.name, last.author.alternateName];
-    const altLicense = [last.license.name, last.license.alternateName];
-    const altHeadline = [quote(last.headline),
-      quote(last.alternativeHeadline),
-      last.typeOfWork.name];
-    const headlineByAuthor = [[blk(byAuthorWithHeadlineTempl)],
-      altHeadline, altAuthor, altUnder, altLicense];
-    const headlineByAuthorBest = bestOf(headlineByAuthor);
-    if (!_.isNil(headlineByAuthorBest)) {
-      return runByAuthorWithHeadlineTempl(headlineByAuthorBest);
-    }
-
-    const byAuthorAndLicense = [[blk(byAuthorAndLicenseTempl)],
-      altAuthor, altUnder, altLicense,
-    ];
-    const byAuthorAndLicenseBest = bestOf(byAuthorAndLicense);
-    if (!_.isNil(byAuthorAndLicenseBest)) {
-      return runByAuthorAndLicenseTempl(byAuthorAndLicenseBest);
-    }
-
-    const byAuthor = [[blk(byAuthorTempl)], altAuthor];
-    const byAuthorBest = bestOf(byAuthor);
-    if (!_.isNil(byAuthorBest)) {
-      return runByAuthorTempl(byAuthorBest);
-    }
-
-    return null;
+    const format = _.get(opts, 'format', 'text');
   };
 
   const getAttributionAsText = (history, opts) =>
