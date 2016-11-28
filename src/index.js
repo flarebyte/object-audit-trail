75bb1cc6057fc9b930755dbe033d9202682166de
Olivier Huin <olih@users.noreply.github.com>
2016-11-28 22:53:29 +0000

    Basic attribution with one or two contributors (#1)
    * Retrieve attribution from history
    * Adds support for twitter
    * Adds quote for headline
    * With eslint airbnb
    * Adds custom templating
    * Adds two contributors code
    * Adds twitter and markdown
    * kind of limit for markdown
    * Upgrades eslint babel-eslint
    * Deletes node v.10 and .12 as target

--- src/index.js
+++ src/index.js
@@ -1,7 +1,256 @@
+import _ from 'lodash';
+import pickAltVal from 'pick-alternate-value';
+
+const quote = str => `"${str}"`;
+
+const defaults = {
+  text: {
+    i: {
+      templates: ['"{{headline}}" by {{author}} is licensed under {{license}}',
+        '"{{headline}}" by {{author}} / {{license}}',
+        '{{typeOfWork}} by {{author}} / {{license}}',
+        'By {{author}} / {{license}}', 'By {{author}}'],
+      props: {
+        author: ['i.author.name', 'i.author.alternateName'],
+        license: ['r.license.name', 'r.license.alternateName'],
+        headline: ['r.headline', 'r.alternativeHeadline'],
+        typeOfWork: ['r.typeOfWork.name'],
+      },
+      placeholders: {
+        clean: [],
+        extract: [['{{', '}}']],
+      },
+
+    },
+    ii: {
+      templates: ['"{{headline1}}" by {{author1}} is a derivative of ' +
+      '"{{headline2}}" by {{author2}} / {{license}}'],
+      props: {
+        author1: ['i.author.name', 'i.author.alternateName'],
+        author2: ['ii.author.name', 'ii.author.alternateName'],
+        license: ['r.license.name', 'r.license.alternateName'],
+        headline1: ['r.headline', 'r.alternativeHeadline'],
+        headline2: ['ii.headline', 'ii.alternativeHeadline'],
+      },
+      placeholders: {
+        clean: [],
+        extract: [['{{', '}}']],
+      },
+    },
+  },
+  markdown: {
+    i: {
+      templates: ['[{{headline}}]({{headlineUrl}}) by ' +
+      '[{{author}}]({{authorUrl}}) is licensed under [{{license}}]({{licenseUrl}})',
+        '[{{headline}}]({{headlineUrl}}) by ' +
+      '[{{author}}]({{authorUrl}}) / [{{license}}]({{licenseUrl}})',
+        '[{{typeOfWork}}]({{headlineUrl}}) by ' +
+        '[{{author}}]({{typeOfWorkUrl}}) / [{{license}}]({{licenseUrl}})',
+        'By [{{author}}]({{authorUrl}}) / [{{license}}]({{licenseUrl}})',
+        'By [{{author}}]({{authorUrl}})'],
+      props: {
+        author: ['i.author.name', 'i.author.alternateName'],
+        authorUrl: ['i.author.url'],
+        license: ['r.license.name', 'r.license.alternateName'],
+        licenseUrl: ['r.license.url'],
+        headline: [quote, 'r.headline', 'r.alternativeHeadline'],
+        headlineUrl: ['r.url'],
+        typeOfWork: ['r.typeOfWork.name'],
+        typeOfWorkUrl: ['r.typeOfWork.url'],
+      },
+      placeholders: {
+        clean: [['({{', '}})']],
+        extract: [['[{{', '}}]']],
+      },
+
+    },
+    ii: {
+      templates: ['[{{headline1}}]({{headline1Url}}) by [{{author1}}]({{author1Url}}) is a derivative of ' +
+      '[{{headline2}}]({{headline2Url}}) by [{{author2}}]({{author2Url}}) / [{{license}}]({{licenseUrl}})'],
+      props: {
+        author1: ['i.author.name', 'i.author.alternateName'],
+        author1Url: ['i.author.url'],
+        author2: ['ii.author.name', 'ii.author.alternateName'],
+        author2Url: ['ii.author.url'],
+        license: ['r.license.name', 'r.license.alternateName'],
+        licenseUrl: ['r.license.url'],
+        headline1: [quote, 'r.headline', 'r.alternativeHeadline'],
+        headline1Url: ['r.url'],
+        headline2: [quote, 'ii.headline', 'ii.alternativeHeadline'],
+        headline2Url: ['ii.url'],
+      },
+      placeholders: {
+        clean: [['({{', '}})']],
+        extract: [['[{{', '}}]'], ['({{', '}})']],
+      },
+    },
+  },
+  twitter: {
+    i: {
+      templates: [
+        '"{{headline}}" by {{authorTwitter}} {{licenseHashtag}}: {{headlineUrl}}',
+      ],
+      props: {
+        author: ['i.author.name', 'i.author.alternateName'],
+        authorTwitter: ['i.author.twitter:username'],
+        authorUrl: ['i.author.url'],
+        license: ['r.license.name', 'r.license.alternateName'],
+        licenseUrl: ['r.license.url'],
+        licenseHashtag: ['r.license.twitter:hastag'],
+        headline: ['r.headline', 'r.alternativeHeadline'],
+        headlineUrl: ['r.url'],
+        typeOfWork: ['r.typeOfWork.name'],
+        typeOfWorkUrl: ['r.typeOfWork.url'],
+      },
+      placeholders: {
+        clean: [],
+        extract: [['{{', '}}']],
+      },
+    },
+    ii: {
+      templates: ['"{{headline1}}" by {{author1Twitter}} is a derivative of ' +
+      '"{{headline2}}" by {{author2Twitter}} {{licenseHashtag}}: {{headline1Url}}'],
+      props: {
+        author1: ['i.author.name', 'i.author.alternateName'],
+        author1Twitter: ['i.author.twitter:username'],
+        author1Url: ['i.author.url'],
+        author2: ['ii.author.name', 'ii.author.alternateName'],
+        author2Twitter: ['ii.author.twitter:username'],
+        author2Url: ['ii.author.url'],
+        license: ['r.license.name', 'r.license.alternateName'],
+        licenseUrl: ['r.license.url'],
+        licenseHashtag: ['r.license.twitter:hastag'],
+        headline1: ['r.headline', 'r.alternativeHeadline'],
+        headline1Url: ['r.url'],
+        headline2: ['ii.headline', 'ii.alternativeHeadline'],
+        headline2Url: ['ii.url'],
+      },
+      placeholders: {
+        clean: [],
+        extract: [['{{', '}}']],
+      },
+    },
+  },
+};
+
+
 /**
  * @param {Type}
  * @return {Type}
  */
-export default function () {
-  return true
+export default function (conf) {
+  const uncurie = (path) => {
+    const isNilOrHttp = _.isNil(path) || _.isEmpty(path) ||
+     _.startsWith(path, 'http://') || _.startsWith(path, 'https://');
+    if (isNilOrHttp) {
+      return path;
+    }
+    const hasCurie = _.includes(path, ':');
+    if (!hasCurie) {
+      const pathSuffix = _.trimStart(path, '/');
+      const url = `${conf.baseUrl}/${pathSuffix}`;
+      return url;
+    }
+    const splitted = _.split(path, ':');
+    const prefix = _.head(splitted);
+    const unkownCurie = !_.has(conf.curies, prefix);
+    if (unkownCurie) {
+      return path;
+    }
+    const relUrl = _.tail(splitted).join(':');
+    const curieUrl = conf.curies[prefix];
+    const url = `${curieUrl}/${relUrl}`;
+    return url;
+  };
+
+  const buildTemplating = (templating) => {
+    const decorePaths = (list, key) => {
+      const isUrl = _.endsWith(key, 'Url');
+      if (isUrl) {
+        list.unshift(uncurie);
+      }
+      return list;
+    };
+
+    const props = _.mapValues(templating.props, decorePaths);
+    return { props,
+      templates: templating.templates,
+      placeholders: templating.placeholders,
+    };
+  };
+
+  const buildVariations = variations => _.mapValues(variations, buildTemplating);
+  const buildAllTemplating = v => _.mapValues(v, buildVariations);
+
+  const customDefaults = _.defaults(_.get(conf, 'custom', {}), defaults);
+
+  const custom = buildAllTemplating(customDefaults);
+
+  const onlyMajor = value => !_.includes(
+      conf.ignoreTypeOfContribution,
+      value.typeOfContribution.name);
+
+
+  const onlyMajorContributions = history => _.filter(history, onlyMajor);
+
+  const listAuthors = history => _.uniqBy(_.map(history, 'author'), 'name');
+  const hasSingleMajorContributor = history =>
+  _.size(listAuthors(onlyMajorContributions(history))) === 1;
+
+  const copyeditedContributionAfter = (history, url) => {
+    let edition = null;
+    for (let i = 0; i < history.length; i += 1) {
+      const ed = history[i];
+      if (ed.url === url) {
+        edition = ed;
+        continue;
+      }
+      if (edition === null) continue;
+      if (onlyMajor(ed)) break;
+      edition = ed;
+    }
+    return edition;
+  };
+
+  const getSingleAuthorAttribution = (history, templating, limit) => {
+    const r = _.last(history);
+    const majorContribs = onlyMajorContributions(history);
+    const i = _.last(majorContribs);
+    return pickAltVal.renderLongest(templating, { r, i }, limit);
+  };
+
+  const getTwoAuthorsAttribution = (history, templating, limit) => {
+    const r = _.last(history);
+    const majorContribs = onlyMajorContributions(history);
+    const i = _.last(majorContribs);
+    const lastAuthor = _.get(i, 'author.name');
+    const exceptLastAuthor = c => _.get(c, 'author.name') !== lastAuthor;
+    const contribsOther = _.filter(majorContribs, exceptLastAuthor);
+    const ii = _.last(contribsOther);
+    return pickAltVal.renderLongest(templating, { r, i, ii }, limit);
+  };
+
+
+  const getAttribution = (history, opts) => {
+    const limit = _.get(opts, 'limit', 10000);
+    const format = _.get(opts, 'format', 'text');
+
+    const templatings = _.get(custom, format);
+    const single = hasSingleMajorContributor(history);
+    const attr = single ?
+    getSingleAuthorAttribution(history, templatings.i, limit) :
+    getTwoAuthorsAttribution(history, templatings.ii, limit);
+
+    return _.isNil(attr) && !single ?
+     getSingleAuthorAttribution(history, templatings.i, limit) :
+     attr;
+  };
+
+  const objectAuditTrail = {
+    uncurie,
+    onlyMajorContributions,
+    copyeditedContributionAfter,
+    getAttribution,
+  };
+  return objectAuditTrail;
 }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,281 @@
-import test from "tape"
-import objectAuditTrail from "../src"
+import test from 'tape';
+import objectAuditTrail from '../src';
 
-test("objectAuditTrail", (t) => {
-  t.plan(1)
-  t.equal(true, objectAuditTrail(), "return true")
-})
+const oneAuthor = require('./one-author.json');
+const secondAuthor = require('./second-author.json');
+const author3 = require('./third-author.json');
+const author4 = require('./fourth-author.json');
+const author5 = require('./fifth-author.json');
+
+/*
+See https://wiki.creativecommons.org/wiki/Best_practices_for_attribution
+*/
+
+const tested = objectAuditTrail({
+  curies: {
+    wikipedia: 'http://wikipedia/about',
+  },
+  baseUrl: 'http:/website.com',
+  ignoreTypeOfContribution: ['minor-change'],
+});
+
+test('objectAuditTrail should extract the major contributions', (t) => {
+  t.plan(4);
+  const hist = [
+    {
+      author: { name: 'A' },
+      typeOfContribution: { name: 'W1' },
+    },
+    {
+      author: { name: 'A1' },
+      typeOfContribution: { name: 'minor-change' },
+    },
+    {
+      author: { name: 'B' },
+      typeOfContribution: { name: 'W2' },
+    },
+    {
+      author: { name: 'C' },
+      typeOfContribution: { name: 'W3' },
+    },
+    {
+      author: { name: 'D' },
+      typeOfContribution: { name: 'minor-change' },
+    },
+    {
+      author: { name: 'E' },
+      typeOfContribution: { name: 'minor-change' },
+    },
+  ];
+  t.equal(tested.onlyMajorContributions(hist).length, 3, 'size');
+  t.equal(tested.onlyMajorContributions(hist)[0].author.name, 'A', 'A');
+  t.equal(tested.onlyMajorContributions(hist)[1].author.name, 'B', 'B');
+  t.equal(tested.onlyMajorContributions(hist)[2].author.name, 'C', 'C');
+});
+
+test('objectAuditTrail should extract the copy edited contribution', (t) => {
+  t.plan(4);
+  const hist = [
+    {
+      url: 'A',
+      typeOfContribution: { name: 'W1' },
+    },
+    {
+      url: 'A1',
+      typeOfContribution: { name: 'minor-change' },
+    },
+    {
+      url: 'B',
+      typeOfContribution: { name: 'W2' },
+    },
+    {
+      url: 'C',
+      typeOfContribution: { name: 'W3' },
+    },
+    {
+      url: 'D',
+      typeOfContribution: { name: 'minor-change' },
+    },
+    {
+      url: 'E',
+      typeOfContribution: { name: 'minor-change' },
+    },
+  ];
+  t.equal(tested.copyeditedContributionAfter(hist, 'A').url, 'A1', 'A1');
+  t.equal(tested.copyeditedContributionAfter(hist, 'B').url, 'B', 'B');
+  t.equal(tested.copyeditedContributionAfter(hist, 'C').url, 'E', 'E');
+  t.equal(tested.copyeditedContributionAfter(hist, 'Z'), null, 'Z');
+});
+
+test('objectAuditTrail should uncurie urls', (t) => {
+  t.plan(6);
+  t.equal(tested.uncurie(null), null, 'A');
+  t.equal(tested.uncurie(''), '', 'B');
+  t.equal(tested.uncurie('alpha'), 'http:/website.com/alpha', 'C');
+  t.equal(tested.uncurie('/alpha/beta'), 'http:/website.com/alpha/beta', 'D');
+  t.equal(tested.uncurie('http://bbc.com/story'), 'http://bbc.com/story', 'E');
+  t.equal(tested.uncurie('wikipedia:abc'), 'http://wikipedia/about/abc', 'F');
+});
+
+
+test('objectAuditTrail should provide attribution', (t) => {
+  t.plan(1);
+  const actual = tested.getAttribution([oneAuthor]);
+  t.equal(actual,
+  '"London gathering" by John Smith is licensed under CC BY 4.0',
+  'as text');
+});
+
+test('objectAuditTrail should provide attribution within large limit', (t) => {
+  t.plan(1);
+  const opts = {
+    limit: 100,
+  };
+
+  const actual = tested.getAttribution([oneAuthor], opts);
+  t.equal(actual,
+  '"London gathering" by John Smith is licensed under CC BY 4.0',
+  'as text with large limit');
+});
+
+test('objectAuditTrail should provide compact attribution', (t) => {
+  t.plan(1);
+  const opts = {
+    limit: 40,
+  };
+
+  const actual = tested.getAttribution([oneAuthor], opts);
+  t.equal(actual,
+  '"London gathering" by John Smith / CC BY',
+  'compact license');
+});
+
+
+test('objectAuditTrail should provide short attribution', (t) => {
+  t.plan(1);
+  const opts = {
+    limit: 35,
+  };
+
+  const actual = tested.getAttribution([oneAuthor], opts);
+  t.equal(actual,
+  'Comic Script by John Smith / CC BY',
+  'as short text');
+});
+
+test('objectAuditTrail should provide attribution with just author and license',
+ (t) => {
+   t.plan(1);
+   const opts = {
+     limit: 22,
+   };
+
+   const actual = tested.getAttribution([oneAuthor], opts);
+   t.equal(actual,
+  'By John Smith / CC BY',
+  'with just author and license');
+ });
+test('objectAuditTrail should provide attribution with just author', (t) => {
+  t.plan(1);
+  const opts = {
+    limit: 15,
+    format: 'text',
+  };
+
+  const actual = tested.getAttribution([oneAuthor], opts);
+  t.equal(actual,
+  'By John Smith',
+  'with just author');
+});
+
+test('objectAuditTrail should return empty null limit is too small ', (t) => {
+  t.plan(1);
+  const opts = {
+    limit: 5,
+  };
+
+  const actual = tested.getAttribution([oneAuthor], opts);
+  t.equal(actual,
+  null,
+  'limit is too small');
+});
+
+test('objectAuditTrail should provide markdown attribution', (t) => {
+  t.plan(1);
+  const opts = {
+    format: 'markdown',
+  };
+  const actual = tested.getAttribution([oneAuthor], opts);
+  t.equal(actual,
+  '["London gathering"](http:/website.com/comic-script/AAA) '
+  + 'by [John Smith](http:/website.com/john-smith) is licensed under ' +
+  '[CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)',
+  'as markdown');
+});
+
+test('objectAuditTrail should provide markdown attribution with limit', (t) => {
+  t.plan(1);
+  const opts = {
+    format: 'markdown',
+    limit: 25,
+  };
+  const actual = tested.getAttribution([oneAuthor], opts);
+  t.equal(actual,
+  'By [John Smith](http:/website.com/john-smith) / ' +
+  '[CC BY](https://creativecommons.org/licenses/by/4.0/)');
+});
+
+
+test('objectAuditTrail should provide twitter attribution', (t) => {
+  t.plan(1);
+  const opts = {
+    format: 'twitter',
+  };
+  const actual = tested.getAttribution([oneAuthor], opts);
+  t.equal(actual,
+  '"London gathering" by @johnsmith #CCBY: ' +
+  'http:/website.com/comic-script/AAA',
+  'as twitter');
+});
+
+test('objectAuditTrail should provide attribution with 2 authors', (t) => {
+  t.plan(1);
+  const actual = tested.getAttribution([
+    author3, oneAuthor, author4, secondAuthor, author5]);
+  t.equal(actual,
+  '"alternative Headline 5" by Adele Smith is a derivative of "London gathering" by John Smith / CC BY 4.0',
+  'by 2 authors as text');
+});
+
+test('objectAuditTrail should provide markdown attribution with 2 authors', (t) => {
+  t.plan(1);
+  const opts = {
+    format: 'markdown',
+  };
+  const actual = tested.getAttribution([
+    author3, oneAuthor, author4, secondAuthor, author5], opts);
+  t.equal(actual,
+  '["alternative Headline 5"](http:/website.com/comic-script/EEE) ' +
+  'by [Adele Smith](http:/website.com/adele-smith) is a derivative of ' +
+  '["London gathering"](http:/website.com/comic-script/AAA) by ' +
+  '[John Smith](http:/website.com/john-smith) / ' +
+  '[CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)',
+  'by 2 authors as text');
+});
+
+test('objectAuditTrail should provide twitter attribution', (t) => {
+  t.plan(1);
+  const opts = {
+    format: 'twitter',
+  };
+  const actual = tested.getAttribution([
+    author3, oneAuthor, author4, secondAuthor, author5], opts);
+  t.equal(actual,
+  '"alternative Headline 5" by @adelesmith is a derivative of "London gathering" by @johnsmith #CCBY: ' +
+  'http:/website.com/comic-script/EEE',
+  'as twitter');
+});
+
+test('objectAuditTrail should provide attribution to only 1 author out of 2', (t) => {
+  const opts = {
+    limit: 40,
+  };
+  t.plan(1);
+  const actual = tested.getAttribution([
+    author3, oneAuthor, author4, secondAuthor, author5], opts);
+  t.equal(actual,
+  '"headline 5" by Adele Smith / CC BY 4.0');
+});
+
+test('objectAuditTrail should provide markdown to only 1 author out of 2', (t) => {
+  t.plan(1);
+  const opts = {
+    format: 'markdown',
+    limit: 50,
+  };
+  const actual = tested.getAttribution([
+    author3, oneAuthor, author4, secondAuthor, author5], opts);
+  t.equal(actual,
+  '["headline 5"](http:/website.com/comic-script/EEE) by [Adele Smith](http:/website.com/adele-smith)' +
+  ' / [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)');
+});
